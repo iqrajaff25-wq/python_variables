72ef977b6640006c4f28d2719c5b1d09658d1921
iqra <iqrajaff25@gmail.com>
2025-10-25 00:20:53 +0500

    Arithmetic Operators

--- Arithmetic_Operators_04.py
+++ Arithmetic_Operators_04.py
@@ -1,35 +1,42 @@
 #Assignment#01
 
-total_members = int(input("Enter Total Famil Members "))
+total_members = int(input("Enter Total Family Members: "))
 
 #Income Detail
-father_income = int(input("Enter father_income "))
-mother_income = int(input("Enter mother_income "))
-side_business_income = int(input("Enter side_business_income "))
-other_income = int(input("Enter other_income "))
+print('''
+ENTER INCOME DETAILS;
+---------------------''')
+father_income = int(input("Enter Father's Income: "))
+mother_income = int(input("Enter Mother's Income: "))
+side_business_income = int(input("Enter Side Business Income: "))
+other_income = int(input("Enter Other Income: "))
 total_income = father_income + mother_income + side_business_income + other_income
-print(f"Total Income = {total_income}")
+print(f'TOTAL INCOME = {total_income}')
 
-#Expenses Detail
-house_rent = int(input("Enter house_rent "))
-utilities = int(input("Enter utilities "))
-groceries = int(input("Enter groceries "))
-transport = int(input("Enter transport "))
-education = int(input("Enter education "))
-entertainment = int(input("Enter entertainment "))
-miscellaneous = int(input("Enter miscellaneous "))
+# #Expenses Detail
+print('''ENTER EXPENDITURE DETAILS;
+------------------------''')
+house_rent = int(input("Enter house rent: "))
+utilities = int(input("Enter Utilities: "))
+groceries = int(input("Enter Groceries: "))
+transport = int(input("Enter Transport: "))
+education = int(input("Enter Education: "))
+entertainment = int(input("Enter Entertainment: "))
+miscellaneous = int(input("Enter Miscellaneous: "))
 total_expenses = house_rent + utilities + groceries + transport + education + entertainment + miscellaneous
-print(f"Total Expenses = {total_expenses}")
+print(f'''TOTAL EXPENSES = {total_expenses}
+    ''')
 
 
-#Requirements:
+print('''REQUIREMENTS:
+-------------''')
 total_savings = total_income-total_expenses
-print(f"Total Savings = {total_savings}")
+print(f"TOTAL SAVINGS = {total_savings}")
 average_income_per_member = total_income / total_members
-print(f"Average Income Per Member = {average_income_per_member}")
+print(f"AVERAGE INCOME PER MEMBER = {average_income_per_member}")
 average_expense_per_member = total_expenses / total_members
-print(f"Average Expense Per Member = {average_expense_per_member}")
+print(f"AVERAGE EXPENSE PER MEMBER = {average_expense_per_member}")
 saving_per_member = total_savings / total_members
-print(f"Saving Per Member = {saving_per_member}")
+print(f"SAVING PER MEMBER = {saving_per_member}")
 percentage_of_saving = total_savings / total_income * 100
-print(f"Percentage of Savings = {percentage_of_saving}")
+print(f"PERCENTAGE OF SAVINGS = {percentage_of_saving}%")
