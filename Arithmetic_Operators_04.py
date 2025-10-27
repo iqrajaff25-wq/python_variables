d9c8cda318e94b3fe12b9b1cfb9d4793bda9a519
iqra <iqrajaff25@gmail.com>
2025-10-27 13:49:51 +0500

    Arithmetic Operators added

--- Arithmetic_Operators_04.py
+++ Arithmetic_Operators_04.py
@@ -1,2 +1,50 @@
 #ARITHMETIC OPERATORS
-#
+#Addition  ("+")
+sum1 = 10
+sum2 = 20
+sum = sum1 + sum2
+print(f"Sum of {sum1} and {sum2} is {sum}")
+
+
+#Subtraction  ("-")
+sub1 = 30
+sub2 = 50
+subtract = sub1 - sub2
+subtract2 = sub2- sub1
+print(f"Subtraction of {sub1} and {sub2} is {subtract}")
+print(f"Subtraction of {sub1} from {sub2} is {subtract2}")
+
+
+#Multiplication  (" * ")
+mul1 = 20
+mul2 = 50
+multi = mul1 * mul2
+print(f"Multiplication of {mul1} and {mul2} is {multi}")
+
+
+#Division  (" / ")
+div1 =20
+div2 = 10
+division = div1 / div2
+division2 = div2 /div1
+print(f"Division of {div1} by {div2} is {division}")
+print(f"Division of {div2} by {div1} is {division2}")
+
+#Floor Division  (" // ")
+div3 = 30
+div4 = 40
+floor_div = div3 // div4
+print(f"The Floor Division of {div3} by {div4} is {floor_div}")
+
+
+#Modulus  (" % ")
+mod1 = 20
+mod2 = 6
+modulus = mod1 % mod2
+print(f"The Modulus of {mod1} by {mod2} is {modulus}")
+
+
+#Exponentation
+expo1 = 10
+expo2 = 2
+exponentation = expo1 ** expo2
