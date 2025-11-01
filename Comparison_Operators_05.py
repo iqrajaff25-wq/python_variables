1b201fe521311f14fb53898c5ad1f8fcb457bdd5
iqra <iqrajaff25@gmail.com>
2025-11-01 13:31:07 +0500

    Comparison Oprators added

---
+++ Comparison_Operators_05.py
@@ -0,0 +1,16 @@
+#Equal to ( == )
+num1 = 20
+num2 = 20
+equality = num1 == num2
+print(f"{num1} is equal to {num2} : {equality}")
+
+
+#Not-Equal to ( != )
+num3 = 20
+num4 = 30
+not_equal = num3 != num4
+print(f"{num3} is not equal to {num4} : {not_equal}")
+
+
+#Greater Than ( > )
+greater = num4 > num3
