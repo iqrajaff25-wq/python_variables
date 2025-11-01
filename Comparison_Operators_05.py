5d6cc6fd56f9a879f0399d41062b4214c36f1488
iqra <iqrajaff25@gmail.com>
2025-11-01 19:49:03 +0500

    Comparison Operator updated

--- Comparison_Operators_05.py
+++ Comparison_Operators_05.py
@@ -6,11 +6,39 @@ print(f"{num1} is equal to {num2} : {equality}")
 
 
 #Not-Equal to ( != )
-num3 = 20
-num4 = 30
-not_equal = num3 != num4
-print(f"{num3} is not equal to {num4} : {not_equal}")
+salary = 200000
+pension = 75000
+not_equal = salary != pension
+print(f"{salary} is not equal to {pension} : {not_equal}")
 
 
 #Greater Than ( > )
-greater = num4 > num3
+greater = salary > pension
+print(f"{salary} is greater than {pension} : {greater}")
+
+
+#Greater Than Equal to ( >= )
+greater_equal = salary >= pension
+print(f"{salary} is greater equal to {pension} : {greater_equal}")
+if salary >= pension:
+    print("Condition is true")
+else:
+    print("condition is false")
+
+
+#Less Than ( < )
+lesser = pension < salary
+print(f"{pension} is lesser than {salary} : {lesser}")
+
+
+#Lesser_than equalto ( <= )
+less_equal = pension <= salary
+print(f"{pension} is less than equal to {salary} : {less_equal}")
+
+
+#AND Operator
+age = 50
+if age >= 18 and age <= 65:
+    print("You are eligible for entry test")
+else:
+    print("You are not eligible!")
