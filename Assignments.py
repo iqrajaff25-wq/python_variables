c663f8918139e948d7e754b0e8324177c40c5606
iqra <iqrajaff25@gmail.com>
2025-11-07 17:24:51 +0500

    Assignments Updated

--- Assignments.py
+++ Assignments.py
@@ -41,3 +41,39 @@ saving_per_member = total_savings / total_members
 print(f"SAVING PER MEMBER = {saving_per_member}")
 percentage_of_saving = total_savings / total_income * 100
 print(f"PERCENTAGE OF SAVINGS = {percentage_of_saving}%")
+
+
+
+#Assignment # 03
+#  Battery Health Analyzer
+#  Objective: To analyze phone battery status based on percentage and charging condition.
+
+#  Requirements:
+#  1. Ask the user for current battery percentage (0–100) and whether the phone is charging
+#  (yes/no).
+#  2. Use only if-else, logical, and comparison operators to print the correct message.
+#  Formula:
+#  Logical conditions used for decision making (e.g., If (Battery ≤ 10) and (Charging == 'no')).
+#  Conditions:- Battery ≤ 10 and not charging →  Battery Critical – Plug in immediately!- 
+#  Battery ≤ 10 and charging →  Charging started just in time!-
+#  10 < Battery ≤ 30 and not charging → Battery Low – Consider charging soon. 
+#  30 < Battery ≤ 60 and not charging →  Battery OK – Moderate usage allowed.
+#  60 < Battery ≤ 90 and charging →  Battery good – you can unplug soon. 
+#  Battery > 90 and charging →  Battery Full – Unplug charger to save energy. 
+#  Otherwise →  Battery sufficient – all good!
+
+
+def battery_analyzer()
+    battery = int(input("Enter Battery's Current Percentage! (0-100) "))
+    charging = input("Is the device charging? (yes/no): ").lower
+    if battery < 0 or battery >=100:
+        print("Invalid Percentage Entered!")
+    elif battery <= 10 and charging == no:
+        print("Battery Health is Critical. Charge immediately!")
+    elif battery<=10 and charging == yes:
+        print("Charging Started Just In Time")
+    elif battery>10 and battery<=30 and charging == no:
+        print("Battery is low. Consider Charging Soon!")
+    
+    
+
