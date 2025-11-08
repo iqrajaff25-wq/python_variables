32ec28246c955e6ed7da8512f65593a77c582535
iqra <iqrajaff25@gmail.com>
2025-11-08 22:56:58 +0500

    Functions

---
+++ Functions.py
@@ -0,0 +1,43 @@
+#ChatGPT said:
+
+# Types of functions:
+
+# Built-in Functions
+# User-defined Functions
+# Lambda (Anonymous) Functions
+# Recursive Functions
+# Higher-order Functions
+# Generator Functions
+# Nested Functions
+# Decorator Functions
+
+# 01_Built-in Functions
+# These are pre-defined in Python
+    # print()     # Displays output
+    # len()       # Returns length of a sequence
+    # type()      # Returns the type of an object
+    # range()     # Generates a sequence of numbers
+    # sum()       # Returns the sum of all items in an iterable
+    # max()
+    # min()
+    # sorted()
+
+# 02_User-defined Functions
+# Functions that you create using the def keyword.
+def sum():
+    num1 = int(input('Enter first number: '))
+    num2 = int(input("Enter second number: "))
+    total= num1 + num2
+    print(f"The Sum of {num1} & {num2} is {total}")
+
+# can be called like:
+sum()
+
+
+# 03_Lambda (Anonymous) Functions
+#These are one-line, nameless functions created with the lambda keyword.
+#Oftenly used for short & simple tasks.
+
+
+
+
